9b2b4f15c350c570957e73afc24c7b9c30205ada
petritmekuli <petritmekuli2013@gmail.com>
2024-11-13 00:22:58 +0100

    create basic Tasks environment

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'title' => fake()->sentence(5),
+            'description' => fake()->paragraph(5),
+            'status' => fake()->boolean(),
+            'priority' => fake()->numberBetween(1, 3),
+        ];
+    }
+}
